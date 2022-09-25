9cb69def452c5534021d716dfaf39f34af8b0251
Shainal <shainalchetty@gmail.com>
2022-09-25 06:13:58 +0200

    Added created_at_i col to posts table

--- migrations.py
+++ migrations.py
@@ -5,12 +5,20 @@ import sqlite3
 conn = sqlite3.connect('hn.db')
 cur = conn.cursor()
 
+
+#--------- DB Schema Update Functions ---------
+
+
 def schema_update_1():
     cur.execute('CREATE TABLE posts(Post_ID PRIMARY KEY, Title, Story_Text, Creation_date, Num_Comments);')
 
+def schema_update_2():
+    cur.execute('ALTER TABLE posts ADD created_at_i')
 
+#--------- Updates ---------
 
-
+#schema_update_1()
+schema_update_2()
 
 
 
